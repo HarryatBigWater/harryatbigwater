e02fd7f159d5b09a9b768fbfee36565bc37dd8ad
Harry Maher <harry@internal.bigwaterconsulting.net>
2019-06-06 14:35:11 -0700

    updating sample survey for WMAHA

--- sample_survey/script.js
+++ sample_survey/script.js
@@ -23,23 +23,16 @@ var results  = (function() {
 var question1 = {
   'closetofamily' : 0,
   'quietandpeaceful' : 0,
-  'ilikecorn' : 0,
-  'proximitytoocean' : 0
+  'beautifuldesert' : 0,
+  'hotdogs' : 0
 };
 
 var question2 = {
-  'threetosixinches' : 0,
-  'sixtoeightinches' : 0,
-  'fivefeettoonefoot' : 0,
-  'cm' : 0,
-  'hand': 0,
-};
-
-var question3 = {
-  'reallybig' : 0,
-  'verybig' : 0,
-  'extremelybig' : 0,
-  'unbelievablybig' : 0
+  'ten' : 0,
+  'thirty' : 0,
+  'hour' : 0,
+  'two' : 0,
+  'toolong': 0,
 };
 
 var question4 = {
@@ -48,8 +41,8 @@ var question4 = {
 };
 
 var question5 = {
-  'cornhuskers' : 0,
-  'sooners' : 0
+  'diamondbacks' : 0,
+  'giants' : 0
 };
 
 // console.log(results.records)
@@ -61,6 +54,7 @@ var question5 = {
 
 for (i in results.records) {
     // console.log('POST ' + i)
+  try{  
     console.log(results.records[i].form_values['bcc2'].choice_values[0])
     // console.log(results.records[i].form_values['2355'].choice_values[0])
     // console.log(results.records[i].form_values['00a8'])
@@ -72,53 +66,46 @@ for (i in results.records) {
         case 'quietandpeaceful':
             question1.quietandpeaceful += 1
             break;
-        case 'ilikecorn':
-            question1.ilikecorn += 1
+        case 'beautifuldesert':
+            question1.beautifuldesert += 1
             break;
-        case 'proximitytoocean':
-            question1.proximitytoocean += 1
+        case 'hotdogs':
+            question1.hotdogs += 1
             break;
         default:
             console.log('Not a known value for question1')
     }
+  }
+  catch(err){
+    console.log('Error w/ question1')
+  }
 
+  try{
     switch(results.records[i].form_values['bcc2'].choice_values[0]) {
-      case 'threetosixinches':
-          question2.threetosixinches += 1
+      case 'ten':
+          question2.ten += 1
           break;
-      case 'sixtoeightinches':
-          question2.sixtoeightinches += 1
+      case 'thirty':
+          question2.thirty += 1
           break;
-      case 'fivefeettoonefoot':
-          question2.fivefeettoonefoot += 1
+      case 'hour':
+          question2.hour += 1
           break;
-      case 'cm':
-          question2.cm += 1
+      case 'two':
+          question2.two += 1
           break;
-      case 'hand':
-          question2.hand += 1
+      case 'toolong':
+          question2.toolong += 1
           break;
       default:
           console.log('Not a known value for question2')
     }
-
-    switch(results.records[i].form_values['24e4'].choice_values[0]) {
-      case 'reallybig':
-          question3.reallybig += 1
-          break;
-      case 'verybig':
-          question3.verybig += 1
-          break;
-      case 'extremelybig':
-          question3.extremelybig += 1
-          break;
-      case 'unbelievablybig':
-          question3.unbelievablybig += 1
-          break;
-      default:
-          console.log('Not a known value for question 3')
+  }
+      catch(err){
+    console.log('Error w/ question2')
   }
 
+try{
     switch(results.records[i].form_values['68c6']) {
       case 'yes':
           question4.yes += 1
@@ -129,17 +116,26 @@ for (i in results.records) {
       default:
           question4.other += 1
     }
+  }
+  catch(err){
+    console.log("error w/ question3")
+  }
+
+try{
     switch(results.records[i].form_values['f7a5'].choice_values[0]) {
-      case 'cornhuskers':
-          question5.cornhuskers += 1
+      case 'diamondbacks':
+          question5.diamondbacks += 1
           break;
-      case 'sooners':
-          question5.sooners += 1
+      case 'giants':
+          question5.giants += 1
           break;
       default:
           console.log('Not a known value for question 5')
     }
-
+  }
+  catch(err){
+    console.log("error w/ question4")
+  }
 }
 
 // console.log('after')
@@ -154,9 +150,9 @@ var ctx = document.getElementById("myChart1").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Close to family', 'Quiet and peaceful', 'I like corn', 'Proximity to ocean'],
+        labels: ['Close to family', 'Quiet and peaceful', 'Beautiful deserts', 'Sonoran hot dogs'],
         datasets: [{
-            data: [question1.closetofamily, question1.quietandpeaceful, question1.ilikecorn, question1.proximitytoocean],
+            data: [question1.closetofamily, question1.quietandpeaceful, question1.beautifuldesert, question1.hotdogs],
             backgroundColor: [
               'rgba(255, 159, 64, 0.2)',
               'rgba(0, 0, 0, 0.2)',
@@ -210,9 +206,9 @@ var ctx = document.getElementById("myChart2").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'horizontalBar',
     data: {
-      labels: ["3 to 6 inches", "6 to 8 inches", ".5 feet to one foot", "480cm to 650cm", "The size of my hand"],
+      labels: ["10 Minutes", "30-45 minutes", "An hour", "Two", "Too long"],
         datasets: [{
-            data: [question2.threetosixinches,question2.sixtoeightinches,question2.fivefeettoonefoot,question2.cm,question2.hand],
+            data: [question2.ten,question2.thirty,question2.hour,question2.two,question2.toolong],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -256,45 +252,6 @@ var myChart = new Chart(ctx, {
 
 
 
-var ctx = $("#myChart3");
-var data = {
-    labels: ["Really big", "Very big", "Extremely big", "Unbelievably big"],
-    datasets: [
-        {
-            data: [question3.reallybig,question3.verybig,question3.extremelybig,question3.unbelievablybig],
-            backgroundColor: [
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(0, 0, 0, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-            ],
-            borderColor: [
-              'rgba(75, 192, 192, 1)',
-              'rgba(255, 99, 132, 1)',
-              'rgba(255, 159, 64, 1)',
-              'rgba(0, 0, 0, 1)',
-              'rgba(54, 162, 235, 1)',
-            ],
-            hoverBackgroundColor: [
-              'rgba(75, 192, 192, 0.8)',
-              'rgba(255,99,132, 0.8)',
-              'rgba(255, 159, 64, 0.8)',
-              'rgba(0, 0, 0, 0.8)',
-              'rgba(188, 189, 220, 0.8)'
-            ]
-        }]
-};
-var myPieChart = new Chart(ctx,{
-    type: 'pie',
-    data: data,
-    options: {
-      animation:{
-        animateScale:true
-      }
-    }
-});
-
 var ctx = $("#myChart4");
 var data = {
     labels: ["Yes", "No"],
@@ -329,10 +286,10 @@ var myPieChart = new Chart(ctx,{
 
 var ctx = $("#myChart5");
 var data = {
-    labels: ["Nebraska Cornhuskers", "Oklahoma Sooners"],
+    labels: ["Diamondbacks", "Giants"],
     datasets: [
         {
-            data: [question5.cornhuskers,question5.sooners],
+            data: [question5.diamondbacks,question5.giants],
             backgroundColor: [
               'rgba(75, 192, 192, 0.2)',
               'rgba(255, 99, 132, 0.2)'
