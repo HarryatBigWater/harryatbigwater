ff9dc5d212e9189ab40773b099b84f0b39af9713
Harry Maher <harry@internal.bigwaterconsulting.net>
2019-07-18 14:01:09 -0700

    update for next visit

--- sample_survey/script.js
+++ sample_survey/script.js
@@ -18,13 +18,12 @@ var results  = (function() {
         return json;
 })();
 
-// console.log(results)
+console.log(results)
 
 var question1 = {
   'closetofamily' : 0,
-  'quietandpeaceful' : 0,
-  'beautifuldesert' : 0,
-  'hotdogs' : 0
+  'harvesting' : 0,
+  'beautifuldesert' : 0
 };
 
 var question2 = {
@@ -35,14 +34,23 @@ var question2 = {
   'toolong': 0,
 };
 
-var question4 = {
+var question3 = {
   'yes' : 0,
   'no' : 0,
 };
 
+var question4 = {
+  'cardinals' : 0,
+  'bruins' : 0
+};
+
 var question5 = {
-  'diamondbacks' : 0,
-  'giants' : 0
+  'redchili' : 0,
+  'greenchili' : 0,
+  'hotdog' : 0,
+  'baithaj': 0,
+  "vegan": 0,
+  "hotfood": 0
 };
 
 // console.log(results.records)
@@ -54,32 +62,33 @@ var question5 = {
 
 for (i in results.records) {
     // console.log('POST ' + i)
+
+
+//question1
+// console.log(results.records[i].form_values['015e'].choice_values[0])
   try{  
-    console.log(results.records[i].form_values['bcc2'].choice_values[0])
-    // console.log(results.records[i].form_values['2355'].choice_values[0])
-    // console.log(results.records[i].form_values['00a8'])
 
     switch(results.records[i].form_values['015e'].choice_values[0]) {
         case 'closetofamily':
             question1.closetofamily += 1
             break;
-        case 'quietandpeaceful':
-            question1.quietandpeaceful += 1
+        case 'harvesting':
+            question1.harvesting += 1
             break;
-        case 'beautifuldesert':
+        case 'beautifuldeserts':
             question1.beautifuldesert += 1
             break;
-        case 'hotdogs':
-            question1.hotdogs += 1
-            break;
         default:
-            console.log('Not a known value for question1')
+            console.log('Not a known value for question 1')
+            console.log(results.records[i].form_values['015e'].choice_values[0])
     }
   }
   catch(err){
-    console.log('Error w/ question1')
+    console.log('Error w/ question 1')
   }
 
+
+//question 2
   try{
     switch(results.records[i].form_values['bcc2'].choice_values[0]) {
       case 'ten':
@@ -98,51 +107,89 @@ for (i in results.records) {
           question2.toolong += 1
           break;
       default:
-          console.log('Not a known value for question2')
+          console.log('Not a known value for question 2')
     }
   }
       catch(err){
-    console.log('Error w/ question2')
+    console.log('Error w/ question 2')
   }
 
+
+//question 3
 try{
     switch(results.records[i].form_values['68c6']) {
       case 'yes':
-          question4.yes += 1
+          question3.yes += 1
           break;
       case 'no':
-          question4.no += 1
+          question3.no += 1
           break;
       default:
-          question4.other += 1
+          console.log('Not a known value for question 3')
+          console.log(results.records[i].form_values['68c6'])
     }
   }
   catch(err){
-    console.log("error w/ question3")
+    console.log("error w/ question 3")
   }
 
+
+// question 4
+// console.log(results.records[i].form_values['f7a5'].choice_values[0])
 try{
     switch(results.records[i].form_values['f7a5'].choice_values[0]) {
-      case 'diamondbacks':
-          question5.diamondbacks += 1
+      case 'cardinals':
+          question4.cardinals += 1
           break;
-      case 'giants':
-          question5.giants += 1
+      case 'bruins':
+          question4.bruins += 1
+          break;
+      default:
+          console.log('Not a known value for question 4')
+    }
+  }
+  catch(err){
+    console.log("error w/ question 4")
+  }
+
+// question 5
+// console.log(results.records[i].form_values['ee72'].choice_values[0])
+try{
+    switch(results.records[i].form_values['ee72'].choice_values[0]) {
+      case 'redchili':
+          question5.redchili += 1
+          break;
+      case 'greenchili':
+          question5.greenchili += 1
+          break;
+      case 'hotdog':
+          question5.hotdog += 1
+          break;
+      case 'baithaj':
+          question5.baithaj += 1
+          break;
+      case 'vegan':
+          question5.vegan += 1
+          break;
+      case 'hotfood':
+          question5.hotfood += 1
           break;
       default:
           console.log('Not a known value for question 5')
     }
   }
   catch(err){
-    console.log("error w/ question4")
+    console.log("error w/ question 5")
   }
+
 }
 
 // console.log('after')
 // console.log(question1)
 // console.log(question2)
 // console.log(question3)
-
+// console.log(question4)
+console.log(question5)
 
 
 
@@ -150,9 +197,9 @@ var ctx = document.getElementById("myChart1").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Close to family', 'Quiet and peaceful', 'Beautiful deserts', 'Sonoran hot dogs'],
+        labels: ['Close to family', 'Beautiful deserts', 'Saguaro (bahidaj) harvesting'],
         datasets: [{
-            data: [question1.closetofamily, question1.quietandpeaceful, question1.beautifuldesert, question1.hotdogs],
+            data: [question1.closetofamily, question1.beautifuldesert, question1.harvesting],
             backgroundColor: [
               'rgba(255, 159, 64, 0.2)',
               'rgba(0, 0, 0, 0.2)',
@@ -257,7 +304,7 @@ var data = {
     labels: ["Yes", "No"],
     datasets: [
         {
-            data: [question4.yes, question4.no],
+            data: [question3.yes, question3.no],
             backgroundColor: [
               'rgba(255, 159, 64, 0.2)',
               'rgba(0, 0, 0, 0.2)',
@@ -286,10 +333,10 @@ var myPieChart = new Chart(ctx,{
 
 var ctx = $("#myChart5");
 var data = {
-    labels: ["Diamondbacks", "Giants"],
+    labels: ["Cardinals", "UCLA Bruins"],
     datasets: [
         {
-            data: [question5.diamondbacks,question5.giants],
+            data: [question4.cardinals,question4.bruins],
             backgroundColor: [
               'rgba(75, 192, 192, 0.2)',
               'rgba(255, 99, 132, 0.2)'
@@ -313,6 +360,58 @@ var myPieChart = new Chart(ctx,{
       }
     }
 });
+
+
+var ctx = document.getElementById("myChart6").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+      labels: ["Red Chili", "Green Chili", "Sonoran Hot Dogs", "Baithaj", "Vegan", "Hot food"],
+        datasets: [{
+            data: [question5.greenchili,question5.redchili,question5.hotdog,question5.baithaj,question5.vegan,question5.hotfood],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 99, 132, 0.2)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+          legend: {
+        display: false
+    },
+        scales: {
+            xAxes: [{
+              ticks: {
+                autoSkip: false
+              },
+              scaleLabel: {
+                    display: true,
+                    labelString: 'Responses'
+                  }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    autoSkip: false
+                }
+            }]
+        }
+    }
+});
+
 
 
 
